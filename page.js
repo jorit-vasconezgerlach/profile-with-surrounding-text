bdcd82389e3849cd7f939fb12d829359faa7ac8e
Jorit Vásconez Gerlach <jorit@vasconezgerlach.de>
2021-08-31 21:02:28 +0200

    Create JS

---
+++ page.js
@@ -0,0 +1,5 @@
+text = document.querySelector('.text p');
+text.innerHTML = text.innerHTML.split("").map(
+    (char, i) =>
+    '<span style="transform: rotate('+i*8.2+'deg)">'+char+'</span>'
+).join("")
