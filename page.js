da73bd76db0d64d2826729afaacda0189e013ce6
Jorit Vásconez Gerlach <jorit@vasconezgerlach.de>
2021-08-31 22:02:08 +0200

    Reamade letter circle

--- page.js
+++ page.js
@@ -1,5 +1,5 @@
 text = document.querySelector('.text p');
 text.innerHTML = text.innerHTML.split("").map(
     (char, i) =>
-    '<span style="transform: rotate('+i*8.2+'deg)">'+char+'</span>'
+    '<span style="transform: rotate('+i*8.3+'deg)">'+char+'</span>'
 ).join("")
